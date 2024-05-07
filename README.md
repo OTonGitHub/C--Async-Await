2e2516ef856d07b5d86d20db2eb79e77243a3813
OTonGitHub <ahmed.ifhaam.work@outlook.com>
2024-05-07 06:33:09 +0500

    finish part 1 of 2nd video

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Using .NET version `8.0.300-preview.24203.14`
 #### Commands
 
 - `dotnet new gitignore`
-= `dotnet new sln`
+  = `dotnet new sln`
 - `dotnet new console -lang "c#" -n "One" -f "net8.0" -o src/First/one -d -v diag`
 - `dotnet new console -lang "c#" -n "Twi" -f "net8.0" -o src/First/two -d -v diag`
 - `dotnet new console -lang "c#" -n "Three" -f "net8.0" -o src/First/three -d -v diag`
@@ -15,4 +15,15 @@ Using .NET version `8.0.300-preview.24203.14`
 - `dotnet run --project src/Second`
 
 #### Notes
-- asynchrony process itself invovles letting another process handle it while doing some of its own work.
+
+- Asynchrony process itself invovles letting another process handle it while doing some of its own work.
+- To Run in VSCode, just press F5, if you have C# Extension Installed, and Have a working solution, should work.
+- When Tasks and Async Awaits are needed are really when you need to do I/O bound tasks
+  - like networking,
+  - disk / database.
+- As soon as async is called, a state machine is created, this can be seen in the IL code or use ILSpy to see the difference.
+- Statemachine is like a class, and has points where code is awaited in the function, the variables become properties / states, and is saved in memory and not GC-ed.
+- Don't make first reaction to change function async even if returning a Task/Promise.
+  - Instead use something like `Task.FromResult` or `Task.CompletedTask`.
+- As soon as tasks or async is used, it is propagated throughout the code.
+  - I guess mostly if you are just waiting for something from a Task.
---
+++ src/Second/Controllers/AvoidStateMachine.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Second.Controllers;
+
+public class AvoidStateMachine : Controller
+{
+    private const string cnn = "https://CNN.com";
+
+    /*
+     * Primary lesson: avoid spawning State Machine when not needed.
+     * But, use await, and spawn machine if need to do something with data,
+     * before returning it. 
+    */
+
+    // If other function is needed, will need to await on it.
+    public async Task<ActionResult> Index()
+    {
+        var item = InputOutputNetwork();
+        return Content(item + await InputOutputNetworkAss());
+    }
+
+    public async Task<string> InputOutputNetworkAss()
+    {
+        var client = new HttpClient();
+        // better to just not await, and remove async, and return Task.
+        var content = await client.GetStringAsync(cnn);
+        // do something with content.
+        // which then won't return a task, because its awaited.
+        // since function is async, it expects await
+        // which means it expects return string, not Task<string>.
+        return content; // if async, return must be string, not Task.
+    }
+    // kinda works fine?
+    public Task<string> InputOutputNetwork()
+    {
+        var client = new HttpClient();
+        var content = client.GetStringAsync(cnn);
+        return content;
+    }
+
+    // Have To Implement A Task, Task Is Completed
+    public Task<string> InputOutput()
+    {
+        // works synchronously. async is not used.
+        var message = "Hello Worldy";
+        return Task.FromResult(message);
+    }
+
+    // Have To Implement A Task, Task Is Completed
+    public Task InputOutputCompleted()
+    {
+        return Task.CompletedTask;
+    }
+}
